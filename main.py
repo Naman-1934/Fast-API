a9459bcf059adef15b06910541f140568eac2be1
= <=>
2026-09-18 22:57:52 +0530

    POST Method

--- main.py
+++ main.py
@@ -5,14 +5,56 @@ from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field, computed_field
+from typing import Annotated, Literal
+
+
 # Object of FastAPI.
 app = FastAPI()
+
+
+class Patient(BaseModel):
+
+    id: Annotated[str, Field(..., description='ID of the patient', examples=['P001'])]
+    name: Annotated[str, Field(..., description='Name of the patient')]
+    city: Annotated[str, Field(..., description='City where the patient is living')]
+    age: Annotated[int, Field(..., gt=0, lt=100, description='Age of the patient')]
+    gender: Annotated[Literal['male', 'female', 'other'], Field(..., description='Gender of the patient')]
+    height: Annotated[float, Field(..., gt=0, description='Height of the patient in mtrs')]
+    weight: Annotated[float, Field(..., gt=0, description='Weight of the patient in kgs')]
+
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi = round(self.weight / (self.height ** 2), 2)
+        return bmi
+
+    @computed_field
+    @property
+    def verdict(self) -> str:
+
+        if self.bmi < 18.5:
+            return 'UnderWeight'
+        elif self.bmi < 25:
+            return 'Normal'
+        elif self.bmi < 30:
+            return 'Normal'
+        else:
+            return 'obese'
+
 
 def load_data():
     with open("patients.json", "r") as f:
         data = json.load(f)
 
     return data
+
+def save_data(data):
+    with open('patients.json', 'w') as f:
+
+        # dump data from data dictionay into a file f. r
+        json.dump(data, f)
 
 
 # we use get because we want to get the data from the server.
@@ -72,3 +114,29 @@ def sort_patients(sort_by: str = Query(..., description="Sort on the basis of he
     sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
 
     return sorted_data
+
+
+# patient is a object as a variable and pydantic model checks whether a given data in a proper format and according to the rules and if it is a correct then it will go further.
+@app.post('/create')
+def create_patient(patient:Patient):
+
+    # Load Existing data
+    data = load_data()
+
+    # check if the patient already exists
+    if patient.id in data:
+        raise HTTPException(status_code=400, detail='Patient Already exists.')
+
+    # new patient add to the database
+    # data in a dictionary format and patient is a pydantic object so, we need to add patient obejct into an existed data
+    # We need to convert that patient object into a dictionary
+
+    # model_dump will convert object into a dictionary.
+    data[patient.id] = patient.model_dump(exclude=['id'])
+
+
+    # Save into json file.
+    save_data(data)
+
+    return JSONResponse(status_code=201, content={'message': 'patient created successfully.'})
+
